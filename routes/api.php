aef5c2ecc6be39ede956e58823b720a9f103dffa
Eugene <jonston85@gmail.com>
2026-01-17 01:46:10 +0200

    Add reverb related changes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Events\TestEvent;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,3 +10,8 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::apiResource('products', ProductController::class);
+
+Route::post('/test-event', function () {
+    broadcast(new TestEvent('Hello from Laravel!'));
+    return response()->json(['status' => 'Event broadcasted']);
+});
